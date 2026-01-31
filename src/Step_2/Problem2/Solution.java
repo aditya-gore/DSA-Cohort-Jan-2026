15899ef51819e45305220c127d38856d40cab59e
aditya gore <aditya_gore@hotmail.com>
2026-01-31 09:17:19 +0530

    solved all problems in step 2

---
+++ src/Step_2/Problem2/Solution.java
@@ -0,0 +1,8 @@
+package Step_2.Problem2;
+
+class Solution {
+    public boolean haveConflict(String[] event1, String[] event2) {
+        return event1[0].compareTo(event2[1]) <= 0
+                && event2[0].compareTo(event1[1]) <= 0;
+    }
+}
